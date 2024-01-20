7d114894929b0ed7617d8884a56b7f4e2c84ad96
iamrajeev2005 <fea.rajeev@gmail.com>
2024-01-20 23:27:03 +0530

    active nav links added

--- script.js
+++ script.js
@@ -169,3 +169,21 @@ hoverText1.addEventListener("mouseleave", () => {
 
 servicePageAnim();
 cursorAnim();
+
+const sections = document.querySelectorAll("section")
+const navLinks = document.querySelectorAll("nav a")
+
+window.onscroll = () => [
+  sections.forEach((sec) => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight 
+    let id = sec.getAttribute('id')
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector(`nav a[href*=`+ id +`]`).classList.add('active')
+      })
+    }
+  })
+]
