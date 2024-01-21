4667b58f54e914ca81940b55f8bd5d8864196bee
iamrajeev2005 <fea.rajeev@gmail.com>
2024-01-21 16:49:06 +0530

    magnet effect done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const lerp = (x, y, a) => x * (1 - a) + y * a;
+
 function cursorAnim() {
   const crsr = document.querySelector(".cursor");
   const main = document.querySelector(".main");
@@ -21,6 +23,8 @@ function cursorAnim() {
     gsap.to(crsr, {
       x: e.x + "px",
       y: e.y + "px",
+      duration:.2,
+      ease:Expo
     });
   });
   nav.addEventListener("mouseenter", () => {
@@ -49,125 +53,124 @@ function cursorAnim() {
   });
 }
 
-function servicePageAnim(){
-const servicePage = document.querySelector(".services");
-const aboutPage = document.querySelector(".about");
-const projectsPage = document.querySelector(".projects");
-const hoverText = document.querySelector(".services .hover-texts .hover-top");
-const hoverText1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1"
-);
+function servicePageAnim() {
+  const servicePage = document.querySelector(".services");
+  const aboutPage = document.querySelector(".about");
+  const projectsPage = document.querySelector(".projects");
+  const hoverText = document.querySelector(".services .hover-texts .hover-top");
+  const hoverText1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1"
+  );
 
-const firstWord = document.querySelector(
-  ".services .hover-texts .hover-top .first"
-);
-const secondWord = document.querySelector(
-  ".services .hover-texts .hover-top .second"
-);
-const thirdWord = document.querySelector(
-  ".services .hover-texts .hover-top .third"
-);
-const fourthWord = document.querySelector(
-  ".services .hover-texts .hover-top .fourth"
-);
-const fifthWord = document.querySelector(
-  ".services .hover-texts .hover-top .fifth"
-);
-const sixthWord = document.querySelector(
-  ".services .hover-texts .hover-top .sixth"
-);
-const seventhWord = document.querySelector(
-  ".services .hover-texts .hover-top .seventh"
-);
-const eightWord = document.querySelector(
-  ".services .hover-texts .hover-top .eight"
-);
+  const firstWord = document.querySelector(
+    ".services .hover-texts .hover-top .first"
+  );
+  const secondWord = document.querySelector(
+    ".services .hover-texts .hover-top .second"
+  );
+  const thirdWord = document.querySelector(
+    ".services .hover-texts .hover-top .third"
+  );
+  const fourthWord = document.querySelector(
+    ".services .hover-texts .hover-top .fourth"
+  );
+  const fifthWord = document.querySelector(
+    ".services .hover-texts .hover-top .fifth"
+  );
+  const sixthWord = document.querySelector(
+    ".services .hover-texts .hover-top .sixth"
+  );
+  const seventhWord = document.querySelector(
+    ".services .hover-texts .hover-top .seventh"
+  );
+  const eightWord = document.querySelector(
+    ".services .hover-texts .hover-top .eight"
+  );
 
-const firstWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .first1"
-);
-const secondWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .second1"
-);
-const thirdWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .third1"
-);
-const fourthWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .fourth1"
-);
-const fifthWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .fifth1"
-);
-const sixthWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .sixth1"
-);
-const seventhWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .seventh1"
-);
-const eightWord1 = document.querySelector(
-  ".services .hover-texts1 .hover-top1 .eight1"
-);
+  const firstWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .first1"
+  );
+  const secondWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .second1"
+  );
+  const thirdWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .third1"
+  );
+  const fourthWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .fourth1"
+  );
+  const fifthWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .fifth1"
+  );
+  const sixthWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .sixth1"
+  );
+  const seventhWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .seventh1"
+  );
+  const eightWord1 = document.querySelector(
+    ".services .hover-texts1 .hover-top1 .eight1"
+  );
 
-hoverText.addEventListener("mouseenter", () => {
-  servicePage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  aboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  projectsPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  hoverText.style.color = "White";
-  firstWord.style.transform = "translate(-6vw,-6vw) rotate(-10deg)";
-  secondWord.style.transform = "translate(-2vw,-3vw) rotate(-10deg)";
-  thirdWord.style.transform = "translate(-1vw,3vw) rotate(10deg)";
-  fourthWord.style.transform = "translate(0vw,0vw) rotate(-10deg)";
-  fifthWord.style.transform = "translate(-1vw,5vw) rotate(10deg)";
-  sixthWord.style.transform = "translate(-1vw,-1vw) rotate(6deg)";
-  seventhWord.style.transform = "translate(-1vw,1vw) rotate(-8deg)";
-  eightWord.style.transform = "translate(-1vw,1vw) rotate(-10deg)";
-});
-hoverText.addEventListener("mouseleave", () => {
-  servicePage.style.backgroundColor = "white";
-  aboutPage.style.backgroundColor = "white";
-  projectsPage.style.backgroundColor = "white";
-  hoverText.style.color = "Black";
-  firstWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  secondWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  thirdWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  fourthWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  fifthWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  sixthWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  seventhWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  eightWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
-});
-hoverText1.addEventListener("mouseenter", () => {
-  servicePage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  aboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  projectsPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  hoverText1.style.color = "White";
-  firstWord1.style.transform = "translate(-6vw,-6vw) rotate(-10deg)";
-  secondWord1.style.transform = "translate(-2vw,-3vw) rotate(-10deg)";
-  thirdWord1.style.transform = "translate(-1vw,3vw) rotate(10deg)";
-  fourthWord1.style.transform = "translate(0vw,0vw) rotate(-10deg)";
-  fifthWord1.style.transform = "translate(-1vw,5vw) rotate(10deg)";
-  sixthWord1.style.transform = "translate(-1vw,-1vw) rotate(6deg)";
-  seventhWord1.style.transform = "translate(-1vw,1vw) rotate(-8deg)";
-  eightWord1.style.transform = "translate(-1vw,1vw) rotate(-10deg)";
-});
-hoverText1.addEventListener("mouseleave", () => {
-  servicePage.style.backgroundColor = "white";
-  aboutPage.style.backgroundColor = "white";
-  projectsPage.style.backgroundColor = "white";
-  hoverText1.style.color = "Black";
-  firstWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  secondWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  thirdWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  fourthWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  fifthWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  sixthWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  seventhWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-  eightWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
-});
-
+  hoverText.addEventListener("mouseenter", () => {
+    servicePage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    aboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    projectsPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    hoverText.style.color = "White";
+    firstWord.style.transform = "translate(-6vw,-6vw) rotate(-10deg)";
+    secondWord.style.transform = "translate(-2vw,-3vw) rotate(-10deg)";
+    thirdWord.style.transform = "translate(-1vw,3vw) rotate(10deg)";
+    fourthWord.style.transform = "translate(0vw,0vw) rotate(-10deg)";
+    fifthWord.style.transform = "translate(-1vw,5vw) rotate(10deg)";
+    sixthWord.style.transform = "translate(-1vw,-1vw) rotate(6deg)";
+    seventhWord.style.transform = "translate(-1vw,1vw) rotate(-8deg)";
+    eightWord.style.transform = "translate(-1vw,1vw) rotate(-10deg)";
+  });
+  hoverText.addEventListener("mouseleave", () => {
+    servicePage.style.backgroundColor = "white";
+    aboutPage.style.backgroundColor = "white";
+    projectsPage.style.backgroundColor = "white";
+    hoverText.style.color = "Black";
+    firstWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    secondWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    thirdWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    fourthWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    fifthWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    sixthWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    seventhWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    eightWord.style.transform = "translate(0vw,0vw) rotate(0deg)";
+  });
+  hoverText1.addEventListener("mouseenter", () => {
+    servicePage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    aboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    projectsPage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    hoverText1.style.color = "White";
+    firstWord1.style.transform = "translate(-6vw,-6vw) rotate(-10deg)";
+    secondWord1.style.transform = "translate(-2vw,-3vw) rotate(-10deg)";
+    thirdWord1.style.transform = "translate(-1vw,3vw) rotate(10deg)";
+    fourthWord1.style.transform = "translate(0vw,0vw) rotate(-10deg)";
+    fifthWord1.style.transform = "translate(-1vw,5vw) rotate(10deg)";
+    sixthWord1.style.transform = "translate(-1vw,-1vw) rotate(6deg)";
+    seventhWord1.style.transform = "translate(-1vw,1vw) rotate(-8deg)";
+    eightWord1.style.transform = "translate(-1vw,1vw) rotate(-10deg)";
+  });
+  hoverText1.addEventListener("mouseleave", () => {
+    servicePage.style.backgroundColor = "white";
+    aboutPage.style.backgroundColor = "white";
+    projectsPage.style.backgroundColor = "white";
+    hoverText1.style.color = "Black";
+    firstWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    secondWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    thirdWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    fourthWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    fifthWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    sixthWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    seventhWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+    eightWord1.style.transform = "translate(0vw,0vw) rotate(0deg)";
+  });
 }
 
-function navAnim(){
+function navAnim() {
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll("nav a");
 
@@ -189,20 +192,114 @@ function navAnim(){
   ];
 }
 
+function lenis() {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+}
+
+function magneticEffect() {
+  const starIcon = document.querySelector(".about .about-text p img");
+  const crsr = document.querySelector(".cursor");
+
+  starIcon.addEventListener("mousemove", (e) => {
+    const dim = starIcon.getBoundingClientRect();
+    const xstart = dim.x;
+    const xend = dim.x + dim.width;
+
+    const ystart = dim.y;
+    const yend = dim.y + dim.height;
+
+    const zeroone = gsap.utils.mapRange(xstart, xend, 0, 1, e.clientX);
+    const zerotwo = gsap.utils.mapRange(ystart, yend, 0, 1, e.clientY);
+
+    gsap.to(crsr, {
+      scale: 3,
+    });
+    gsap.to(starIcon, {
+      x: lerp(-20, 20, zeroone),
+      y: lerp(-20, 20, zerotwo),
+      duration: 0.6,
+    });
+  });
+  starIcon.addEventListener("mouseleave", () => {
+    gsap.to(crsr, {
+      scale: 1,
+    });
+    gsap.to(starIcon, {
+      x: 0,
+      y: 0,
+      duration: 0.6,
+    });
+  });
+}
+
+function magneticEffectTag() {
+  const tag = document.querySelector(".tag ");
+  const crsr = document.querySelector(".cursor");
+
+  tag.addEventListener("mousemove", (e) => {
+    const dim = tag.getBoundingClientRect();
+    const xstart = dim.x;
+    const xend = dim.x + dim.width;
+
+    const ystart = dim.y;
+    const yend = dim.y + dim.height;
+
+    const zeroone = gsap.utils.mapRange(xstart, xend, 0, 1, e.clientX);
+    const zerotwo = gsap.utils.mapRange(ystart, yend, 0, 1, e.clientY);
+
+    gsap.to(crsr, {
+      scale: 3,
+    });
+    gsap.to(tag, {
+      x: lerp(-20, 20, zeroone),
+      y: lerp(-20, 20, zerotwo),
+      duration: 0.6,
+    });
+  });
+  tag.addEventListener("mouseleave", () => {
+    gsap.to(crsr, {
+      scale: 1,
+    });
+    gsap.to(tag, {
+      x: 0,
+      y: 0,
+      duration: 0.6,
+    });
+  });
+}
+
+
+function loaderAnim(){
+
+}
+
+
+
+
+
+
 servicePageAnim();
 cursorAnim();
 navAnim();
+lenis();
+magneticEffect();
+magneticEffectTag();
+loaderAnim();
 
-const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
 
-lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
 
-gsap.ticker.lagSmoothing(0);
+
