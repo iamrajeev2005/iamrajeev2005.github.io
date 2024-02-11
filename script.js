c490ca3675327fa72bc802f1e04378fb634baef2
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-11 12:51:22 +0530

    lenis removed

--- script.js
+++ script.js
@@ -475,17 +475,3 @@ loaderAnim();
 simplePageAnims();
 
 
-
-
-const lenis = new Lenis();
-
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
